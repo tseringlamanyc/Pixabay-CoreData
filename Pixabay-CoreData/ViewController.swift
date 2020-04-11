a649b39557748fbbe111938f24cfb85f6d54b68b
Tsering Lama <tseringlama@pursuit.org>
2020-04-10 20:38:22 -0400

    first commit

---
+++ Pixabay-CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pixabay-CoreData
+//
+//  Created by Tsering Lama on 4/10/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
