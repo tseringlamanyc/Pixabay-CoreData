8891a200e02d29adcce9a4fa92f431bf21c73f0c
Tsering Lama <tseringlama@pursuit.org>
2020-04-13 12:47:42 -0400

    Organizing Folders

---
+++ Pixabay-CoreData/Secrets/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  Pixabay-CoreData
+//
+//  Created by Tsering Lama on 4/13/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import Foundation
+
+struct Apikey {
+    static let key = "14930089-06e7e4452105b9da33a54f058"
+}
